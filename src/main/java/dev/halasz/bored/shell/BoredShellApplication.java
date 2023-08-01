42b9c8c0b6581d4eeb277b301bcac4fbe911c7d0
hal-da <daniel.halasz@gmx.net>
2023-08-01 11:42:15 +0200

    added description for options in help menu

--- src/main/java/dev/halasz/bored/shell/BoredShellApplication.java
+++ src/main/java/dev/halasz/bored/shell/BoredShellApplication.java
@@ -4,15 +4,20 @@ import dev.halasz.bored.shell.Models.BORED_RESPONSE_TYPE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.shell.CompletionContext;
+import org.springframework.shell.command.CommandContext;
 import org.springframework.shell.command.CommandRegistration;
 
 import org.springframework.shell.CompletionProposal;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BoredShellApplication {
-	public final String[] PossibleTypes = BORED_RESPONSE_TYPE.getEnums();
+	public final String[] possibleTypes = BORED_RESPONSE_TYPE.getEnums();
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoredShellApplication.class, args);
@@ -24,19 +29,26 @@ public class BoredShellApplication {
 				.command("get")
 				.description("Get random tipps against boredom.")
 				.withTarget()
-					.function(ctx -> {
-						String arg = ctx.getRawArgs()[ctx.getRawArgs().length-1];
-						return new GetTip().getRequestToBoredAPI(arg);
-					})
+					.function(this::getTipp)
 					.and()
 				.withOption()
-					.completion(ctx -> Stream.of(PossibleTypes)
-							.map(CompletionProposal::new)
-							.collect(Collectors.toList()))
+					.completion(this::getCompletionProposal)
 					.longNames("type")
 					.shortNames('t')
 					.required(false)
+					.description("Possible values: " + Arrays.toString(possibleTypes))
 					.and()
 				.build();
+	}
+
+	private String getTipp(CommandContext ctx) {
+		String arg = ctx.getRawArgs()[ctx.getRawArgs().length-1];
+		return new GetTip().getRequestToBoredAPI(arg);
+	}
+
+	private List<CompletionProposal> getCompletionProposal(CompletionContext ctx) {
+		return Stream.of(possibleTypes)
+				.map(CompletionProposal::new)
+				.collect(Collectors.toList());
 	}
 }
